c1289c43334d29f944b326c22b5a67d55aa9778d
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-06 14:29:00 -0300

    Update

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,8 +1,20 @@
 const express = require ('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+
+const lembretes = {};
+contador = 0;
 
 app.get('/lembretes', (req, res) => {
-    res.status(200).send({"Resultado ": "OK"});
+    //res.status(200).send({"Resultado ": "OK"});
+    res.send(lembretes);
+});
+
+app.put('/lembretes', (req, res) => {
+    // {texto: "Fazer café", id: 1, autor: Ana}
+    const { texto } = req.body;     // Pega somente o texto do json
 });
 
 app.listen(4000, () => {
