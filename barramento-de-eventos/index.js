322bc8c74c7c8ebf5752ecb18ab3b06048931f4f
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-13 14:36:08 -0300

    Implementação do barramento de eventos

---
+++ barramento-de-eventos/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const axios = require('axios');     // import
+
+const app = express();
+app.use(express.json());
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body;
+    // Envia o evento para o microsserviço de lembretes
+    axios.post('https://localhost:4000/eventos', evento);
+    // Envia o evento para o microsserviço de observações
+    axios.post('https://localhost:5000/eventos', evento);
+    res.status(200).send({ msg: 'ok' });
+});
+
+app.listen(10000, () => console.log('Microsserviço Eventu Bus. Porta 10000.'));
