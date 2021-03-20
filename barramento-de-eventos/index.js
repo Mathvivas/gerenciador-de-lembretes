ed5d81528b243a040dcf1d1e02a61d472a0b7e46
Mathvivas <mathvivas@hotmail.com>
2021-03-20 14:59:13 -0300

    Update

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -10,7 +10,9 @@ app.post('/eventos', (req, res) => {
     // Envia o evento para o microsserviço de lembretes
     axios.post('https://localhost:4000/eventos', evento);
     // Envia o evento para o microsserviço de observações
-    axios.post('https://localhost:5000/eventos', evento);
+    axios.post('http://localhost:5000/eventos', evento);
+    // Envia o evento para o microsserviço de consulta
+    axios.post('http://localhost:6000/eventos', evento);
     res.status(200).send({ msg: 'ok' });
 });
 
